4f306704b9740d96a3b5dc511d65c9272edb7577
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-26 19:58:44 +0300

    Add files via upload

--- Generics.ts
+++ Generics.ts
@@ -17,6 +17,7 @@ toArray('a', 'a');
 
 // Constraints
 
+/*
 function len<T extends {length: number}>(arg: T){
     console.log(arg.length);   
 }
@@ -25,8 +26,25 @@ len(['abc']);
 len({length: 10});
 //len(10);
 //len(true);
+*/
+
+// Keyof
+
+type WindowProp = keyof Window;
+
+const myValue: WindowProp = "alert";
+
+interface PC {
+    brand: string;
+    year: number;
+}
+
+type Type1 = keyof PC;
+const val1: Type1 = "brand";
 
 
+type Tuple1 = keyof [string, number];
+const val2: Tuple1 = "0";
 
 
 
